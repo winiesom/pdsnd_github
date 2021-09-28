bdfa1340edf2e525f5f0aa6223f3305837ef2ab7
mmaesom <winifred.esomonu@stetis.com>
2021-09-28 22:59:59 +0100

    documentation: modified bikeshare file

--- bikeshare.py
+++ bikeshare.py
@@ -60,7 +60,7 @@ def load_data(city, month, day):
         (str) month - name of the month to filter by, or "all" to apply no month filter.
         (str) day - name of the day of week to filter by, or "all" to apply no day filter.
     Returns:
-        df - Pandas DataFrame containing city data filtered by month and day
+        df - Pandas DataFrame containing city data filtered by month and day.
     """
     
     # load file in dataframe
